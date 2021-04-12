d0fa569838db8aa02d0bf79607190a7330e4cc67
Kody Wilson <kodywilson@gmail.com>
2021-04-12 16:11:26 -0700

    working with sprites and add example sprites

--- app/main.rb
+++ app/main.rb
@@ -1,144 +1,31 @@
 # test game while learning DragonRuby
+
 def tick args
-# A simple border
-  #                        X   Y   W   H
-  args.outputs.borders << [20, 20, 70, 50]
+  # The frame at which the animation should begin
+  starting_tick = 0
 
-  # A colorful border
-  args.outputs.borders << [
-    60,      # X
-    30,      # Y
-    70,      # W
-    50,      # H
-    lavender # RGB
-  ]
+  # The number of sprites in the animation
+  number_of_sprites = 7
 
-  # A colorful fading border
-  args.outputs.borders << [
-    50,                  # X
-    10,                  # Y
-    50,                  # W
-    40,                  # H
-    fading_crimson(args) # RGBA
-  ]
+  # The number of frames for each sprite
+  # of the animation
+  number_of_frames_per_sprite = 6
 
-  # A simple solid
-  #                        X   Y   W   H
-  args.outputs.solids << [100, 100, 70, 50]
+  # Whether the animation should loop
+  does_animation_loop = true
 
-  # A colorful solid
-  args.outputs.solids << [
-    200,      # X
-    200,      # Y
-    70,      # W
-    50,      # H
-    lavender # RGB
-  ]
-
-  # A colorful fading solid
-  args.outputs.solids << [
-    300,                  # X
-    300,                  # Y
-    50,                  # W
-    40,                  # H
-    fading_crimson(args) # RGBA
-  ]
-
-  # A simple label
-  #                        X   Y   TEXT
-  args.outputs.labels << [5, 85, 'Simple']
-
-  # A small label
-  #                        X   Y   TEXT   SIZE
-  args.outputs.labels << [5, 65, 'Small', -7]
-
-  # A big label
-  #                        X   Y   TEXT  SIZE
-  args.outputs.labels << [5, 55, 'Big',   7]
-
-  # A left-aligned label
-  args.outputs.labels << [
-    args.grid.left.shift_right(5), # X
-    args.grid.bottom.shift_up(10), # Y
-    'Left',                        # TEXT
-    -6,                            # SIZE
-    0                              # ALIGN
-  ]
-
-  # A center-aligned label
-  args.outputs.labels << [
-    args.grid.center.x,            # X
-    args.grid.bottom.shift_up(10), # Y
-    'Center',                      # TEXT
-    -6,                            # SIZE
-    1                              # ALIGN
-  ]
-
-  # A right-aligned label
-  args.outputs.labels << [
-    args.grid.right.shift_left(5), # X
-    args.grid.bottom.shift_up(10), # Y
-    'Right',                       # TEXT
-    -6,                            # SIZE
-    2                              # ALIGN
-  ]
-
-  # A colorful label
-  args.outputs.labels << [
-    args.grid.center.x.shift_right(10), # X
-    args.grid.center.y.shift_up(10),    # Y
-    'Color',                            # TEXT
-    -3,                                 # SIZE
-    0,                                  # ALIGN
-    lavender                            # RGB
-  ]
-
-  # A fading label
-  args.outputs.labels << [
-    args.grid.center.x.shift_right(10), # X
-    args.grid.center.y.shift_down(10),  # Y
-    'Fade',                             # TEXT
-    -3,                                 # SIZE
-    0,                                  # ALIGN
-    fading_crimson(args)                # RGBA
-  ]
-
-  # A simple sprite
+  # A function that calculates the current sprite index
+  sprite_index = starting_tick.frame_index(
+                   number_of_sprites,
+                   number_of_frames_per_sprite,
+                   does_animation_loop
+                 )
+  # Draws the animation
   args.outputs.sprites << [
-    400,                            # X
-    25,                            # Y
-    40,                            # W
-    40,                            # H
-    'sprites/isometric-violet.png' # PATH
+    50,                                      # X
+    20,                                      # Y
+    50,                                      # W
+    50,                                      # H
+    "sprites/misc/explosion-#{sprite_index}.png", # PATH
   ]
-
-  # A rotating sprite
-  args.outputs.sprites << [
-    500,                          # X
-    25,                          # Y
-    40,                          # W
-    40,                          # H
-    'sprites/circle-blue.png',   # PATH
-    args.state.tick_count        # ANGLE
-  ]
-
-  # A fading sprite
-  args.outputs.sprites << [
-    600,                                # X
-    25,                                 # Y
-    40,                                 # W
-    40,                                 # H
-    'sprites/hexagon-orange.png',       # PATH
-    0,                                  # ANGLE
-    255 - args.state.tick_count % 255   # ALPHA
-  ]
-end
-
-def lavender
-  [171, 127, 251]
-end
-
-def fading_crimson args
-  [175, 40, 91,
-    255 - args.state.tick_count % 255]
 end
