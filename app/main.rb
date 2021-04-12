a48bb794147c01982a9549e22bb79221f2771c15
Kody Wilson <kodywilson@gmail.com>
2021-04-12 15:19:50 -0700

    basic primitives

--- app/main.rb
+++ app/main.rb
@@ -2,4 +2,9 @@
 def tick args
   args.outputs.labels  << [640, 500, 'This is really cool!', 5, 1]
   args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+  args.outputs.labels << [100, 100, args.state.tick_count]
+  # draw a blue square that's half way faded out
+  args.outputs.solids << [100, 100, 50, 50, 0, 0, 255, 128]
+  # draw a red label
+  args.outputs.labels << [100, 200, "This is a label.", 255, 0, 0]
 end
