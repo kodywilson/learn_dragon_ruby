a63ddcc24c84f357ca362a534139c225577b040a
Kody Wilson <kodywilson@gmail.com>
2021-04-13 14:23:27 -0700

    add some thumping music

--- app/main.rb
+++ app/main.rb
@@ -5,9 +5,11 @@ def tick args
   args.state.x ||= 620
   args.state.y ||= 250
   ticks = args.state.tick_count
+  @start_tick ||= -60
 
-  if args.inputs.mouse.click
+  if args.inputs.mouse.click && ticks > 10 && @start_tick == -60
     @start_tick = ticks
+    args.outputs.sounds << 'sounds/bass-loops-002-with-drums-short-loop.wav'
   end
 
   if @start_tick + 30 > ticks
@@ -18,10 +20,10 @@ def tick args
     args.state.y -= 3
     args.state.rotation  -= 5
   else
-    @start_tick = 0
+    @start_tick = -60
     args.state.rotation = 0
   end
-
+  #Mist_Covered_Mountains.mp3
   args.outputs.labels << [650, 350, "Jumping Cat! Click to hop!",
   -5, 1, 200, 050, 100, 255] if ticks < 600
   args.outputs.labels << [80, 60, ticks, -5, 0, 200, 050, 100, 125]
