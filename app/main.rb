19e0703d3987a43c673c4db0694ec1b2f8730eea
Kody Wilson <kodywilson@gmail.com>
2021-04-13 12:10:39 -0700

    jumping front flipping cat

--- app/main.rb
+++ app/main.rb
@@ -1,21 +1,32 @@
 # test game while learning DragonRuby
+
 def tick args
   args.state.rotation  ||= 0
   args.state.x ||= 620
   args.state.y ||= 250
+  ticks = args.state.tick_count
 
   if args.inputs.mouse.click
-    args.state.x = args.inputs.mouse.click.point.x - 10
-    args.state.y = args.inputs.mouse.click.point.y - 10
+    @start_tick = ticks
   end
 
-  args.outputs.labels << [650, 350, "Hello World",
-  -5, 1, 200, 050, 100, 255]
-  ticks = args.state.tick_count
+  if @start_tick + 30 > ticks
+    args.state.y += 3
+    args.state.rotation  -= 5
+  elsif
+    @start_tick + 60 > ticks
+    args.state.y -= 3
+    args.state.rotation  -= 5
+  else
+    @start_tick = 0
+    args.state.rotation = 0
+  end
+
+  args.outputs.labels << [650, 350, "Jumping Cat! Click to hop!",
+  -5, 1, 200, 050, 100, 255] if ticks < 600
   args.outputs.labels << [80, 60, ticks, -5, 0, 200, 050, 100, 125]
   args.state.cat_frame = ticks.idiv(4).mod(8)
   args.state.cat_path = "sprites/cat/Run-#{args.state.cat_frame}.png"
   args.outputs.sprites << [args.state.x, args.state.y,
   60, 50, args.state.cat_path, args.state.rotation]
-  args.state.rotation  -= 1
 end
