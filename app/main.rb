8f906209d07522d464911432ad5c15adcf4ad313
Kody Wilson <kodywilson@gmail.com>
2021-04-13 11:17:15 -0700

    sprite rotation

--- app/main.rb
+++ app/main.rb
@@ -1,10 +1,12 @@
 # test game while learning DragonRuby
 def tick args
+  args.state.rotation  ||= 0
   args.outputs.labels << [650, 350, "Hello World",
   -5, 1, 200, 050, 100, 255]
   ticks = args.state.tick_count
   args.outputs.labels << [80, 60, ticks, -5, 0, 200, 050, 100, 125]
   args.state.cat_frame = ticks.idiv(4).mod(8)
   args.state.cat_path = "sprites/cat/Run-#{args.state.cat_frame}.png"
-  args.outputs.sprites << [620, 250, 60, 50, args.state.cat_path]
+  args.outputs.sprites << [620, 250, 60, 50, args.state.cat_path, args.state.rotation]
+  args.state.rotation  -= 1
 end
