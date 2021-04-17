a13e521bd27d84857339d7e76f9cc4f8cddff5f9
Kody Wilson <kodywilson@gmail.com>
2021-04-17 08:55:19 -0700

    add movement

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,8 @@ def tick args
   args.state.rotation  ||= 0
   args.state.x ||= 620
   args.state.y ||= 250
+  args.state.cat_x ||= 200
+  args.state.cat_y ||= 200
   ticks = args.state.tick_count
   @start_tick ||= -60
   @cat_jump_counter ||= 0
@@ -11,6 +13,8 @@ def tick args
 
   if args.inputs.mouse.click && ticks > 10 && @start_tick == -60
     @start_tick = ticks
+    args.state.cat_x += 50
+    args.state.cat_y += 50
     if @bass_loop_counter == -1
       args.outputs.sounds << 'sounds/bass-loops-002-with-drums-short-loop.wav'
       @bass_loop_counter = 1
@@ -22,6 +26,8 @@ def tick args
   else
     @bass_loop_counter = -1
   end
+
+  args.state.x += 0
 
   if @start_tick + 30 > ticks
     args.state.y += 3
@@ -35,11 +41,28 @@ def tick args
     args.state.rotation = 0
   end
   #Mist_Covered_Mountains.mp3
-  args.outputs.labels << [650, 350, "Jumping Cat! Click to hop!",
-  -5, 1, 200, 050, 100, 255] if ticks < 600
+  # Label text as array
+  args.outputs.labels << [670, 400, "Jumping Cat! Click to hop!",
+  0, 1, 050, 050, 200, 255] if ticks < 600
+  # Label text as hash
+  args.outputs.labels << {
+    x: 670,
+    y: 250,
+    text: "Hello Kitty!",
+    size_enum: -3,
+    alignment_enum: 1,
+    r: 200,
+    g: 050,
+    b: 200,
+    a: 255,
+    font: "fonts/manaspc.ttf"
+  }
   args.outputs.labels << [80, 60, ticks, -5, 0, 200, 050, 100, 125]
   args.state.cat_frame = ticks.idiv(4).mod(8)
-  args.state.cat_path = "sprites/cat/Run-#{args.state.cat_frame}.png"
+  args.state.cat_path = "sprites/cat/Walk-#{args.state.cat_frame}.png"
+  args.state.dog_path = "sprites/dog/Slide-#{args.state.cat_frame}.png"
   args.outputs.sprites << [args.state.x, args.state.y,
-  60, 50, args.state.cat_path, args.state.rotation]
+  args.state.cat_x, args.state.cat_y, args.state.cat_path, args.state.rotation]
+  #args.outputs.sprites << [args.state.x, 500,
+  #200, 200, args.state.dog_path, args.state.rotation]
 end
