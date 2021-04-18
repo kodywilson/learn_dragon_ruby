d9106228b8a3e323bc31d734c1096e2f3cc6f46e
Kody Wilson <kodywilson@gmail.com>
2021-04-18 08:21:51 -0700

    very basic bounding

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,7 @@
 # test game while learning DragonRuby
 
 def tick args
+  args.state.click_count ||= 0
   args.state.rotation  ||= 0
   args.state.x ||= 620
   args.state.y ||= 250
@@ -11,7 +12,7 @@ def tick args
   @cat_jump_counter ||= 0
   @bass_loop_counter ||= -1
 
-  if ( args.inputs.mouse.click && ticks > 10 || args.inputs.keyboard.key_held.space ) && @start_tick == -60
+  if ( args.state.click_count >= 3 || args.inputs.keyboard.key_held.space ) && @start_tick == -60
     @start_tick = ticks
     #args.state.cat_x += 50
     #args.state.cat_y += 50
@@ -19,15 +20,16 @@ def tick args
       args.outputs.sounds << 'sounds/bass-loops-002-with-drums-short-loop.wav'
       @bass_loop_counter = 1
     end
+    args.state.click_count = 0
   end
+
+  args.state.click_count += 1 if args.inputs.mouse.up
 
   if @bass_loop_counter > 0 && @bass_loop_counter <= 960
     @bass_loop_counter += 1
   else
     @bass_loop_counter = -1
   end
-
-  args.state.x += 0
 
   if @start_tick + 30 > ticks
     args.state.y += 3
@@ -43,27 +45,27 @@ def tick args
 
   # Move sprite around with keypress
   if args.inputs.keyboard.key_held.right
-    args.state.x += 5
+    args.state.x += 5 unless args.state.x > 1175
   elsif args.inputs.keyboard.key_held.left
-    args.state.x -= 5
+    args.state.x -= 5 if args.state.x > 0
   end
-  #Mist_Covered_Mountains.mp3
+  
   # Label text as array
   args.outputs.labels << [670, 400, "Jumping Cat! Click to hop!",
   0, 1, 050, 050, 200, 255] if ticks < 600
-  # Label text as hash
-  args.outputs.labels << {
-    x: 670,
-    y: 250,
-    text: "Hello Kitty!",
-    size_enum: -3,
-    alignment_enum: 1,
-    r: 200,
-    g: 050,
-    b: 200,
-    a: 255,
-    font: "fonts/manaspc.ttf"
-  }
+  # Label text as hash - example
+  # args.outputs.labels << {
+  #   x: 670,
+  #   y: 250,
+  #   text: "Hello Kitty!",
+  #   size_enum: -3,
+  #   alignment_enum: 1,
+  #   r: 200,
+  #   g: 050,
+  #   b: 200,
+  #   a: 255,
+  #   font: "fonts/manaspc.ttf"
+  # }
   args.outputs.labels << [80, 60, ticks, -5, 0, 200, 050, 100, 125]
   args.state.cat_frame = ticks.idiv(4).mod(8)
   #args.state.cat_path = "sprites/cat/Walk-#{args.state.cat_frame}.png"
